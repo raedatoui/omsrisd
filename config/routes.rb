3975f287b4b2048305c7020302f15e9b7714c92d
Raed Atoui <raed.atoui@gmail.com>
2015-03-15 11:02:22 -0400

    fix project URLs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Risd::Application.routes.draw do
 
   mount OMS::Engine => "/oms"
 
-  get '/interests/:id/:slug' => 'page#node'
+  get '/:token/:id/:slug' => 'page#node'
 
   root to: "page#index"
 
