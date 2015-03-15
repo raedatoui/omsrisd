e45e97ce486960c9360bb59ee0d4510b8f09b55b
Raed Atoui <raed.atoui@gmail.com>
2015-03-15 10:41:20 -0400

    fix sorting and add links to related nodes

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,7 +3,7 @@ class PageController < ApplicationController
   def index
     node_type = OMS::NodeType.find_by_token 'interests'
     @nodes = node_type.nodes.all()
-    @nodes = @nodes.sort_by(&:name)
+    @nodes = @nodes.sort_by{|node| node.name.downcase}
   end
 
   def node
